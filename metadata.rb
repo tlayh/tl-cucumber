9d0a123201015c747bcb2e42a9798bee51e19226
Thomas Layh <thomas@layh.com>
2012-12-02 19:11:55 +0100

    add metadata.rb file

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Thomas Layh"
+maintainer_email "thomas@layh.com"
+license          "MIT"
+description      "Installs/Configures cucumber on a linux system"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
